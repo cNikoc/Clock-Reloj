19a36c1e3f9bb50784018388b9c0a4576a007886
Cristian Nicolás Castagnetto <cncastagnetto@hotmail.com>
2022-05-02 08:57:50 -0300

    backgrounds

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 "use strict";
 
+let body = document.body;
+
 const addZeros = number => {
 	if (number.toString().length < 2) return "0".concat(number);
 	return number;	
@@ -13,6 +15,10 @@ const Clock = ()=> {
 	document.querySelector(".hours").textContent = hours;
 	document.querySelector(".minutes").textContent = minutes;
 	document.querySelector(".seconds").textContent = seconds;
+	if (hours > '05' && hours < '07') body.style.backgroundColor = '#F98577';
+	else if (hours > '07' && hours < '17') body.style.backgroundColor = '#51BEF3';
+	else if (hours > '17' && hours < '19') body.style.backgroundColor = '#F38951';
+	else body.style.backgroundColor = '#222';
 };
 
 Clock();
